196bd5372c8e336ab867b4fd3141cc5ee625ae81
Mikhail Kovbasenko <hankchinaski34@gmail.com>
2021-07-19 12:26:08 +0300

    edited slider component

--- src/redux/reducers/slider.js
+++ src/redux/reducers/slider.js
@@ -1,33 +1,8 @@
-import {SET_SLIDER_HEIGHT} from './../types';
+import {SET_SLIDER_DATA, SET_SLIDER_HEIGHT} from './../types';
 
 
 const initialState = {
-	items: {
-		0: {
-			img: 'free-parking.jpg',
-			title: 'Бесплатная парковка',
-			text: 'Оставляйте машину на платных городских парковках и разрешенных местах, не нарушая ПДД, а также в аэропортах.',
-			color: 'linear-gradient(90deg, #13493F 0%, #0C7B1B 100%)'
-		},
-		1: {
-			img: 'insurance.jpg',
-			title: 'Страховка',
-			text: 'Полная страховка страховка автомобиля',
-			color: 'linear-gradient(90deg, #132949 0%, #0C7B67 100%)'
-		},
-		2: {
-			img: 'benzin.jpg',
-			title: 'Бензин',
-			text: 'Полный бак на любой заправке города за наш счёт',
-			color: 'linear-gradient(90deg, #493013 0%, #7B0C3B 100%)'
-		},
-		3: {
-			img: 'service.jpg',
-			title: 'Обслуживание',
-			text: 'Автомобиль проходит еженедельное ТО',
-			color: 'linear-gradient(90deg, #281349 0%, #720C7B 100%)'
-		},
-	},
+	items: null,
 	sliderHeight: 0,
 	
 }
@@ -40,6 +15,12 @@ const slider = (state = initialState, action) => {
 				sliderHeight: action.data.height
 			}
 		}
+		case SET_SLIDER_DATA: {
+			return {
+				...state,
+				items: action.data.data
+			}
+		}
 		default: return state;
 	}
 }
@@ -50,5 +31,9 @@ export const setSliderHeightActionCreator = height => {
 		data: {height}
 	}
 }
+export const setSliderDataActionCreator = (data) => ({
+	type: SET_SLIDER_DATA,
+	data: {data}
+})
 
 export default slider;
